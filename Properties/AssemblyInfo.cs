126d141e2c5ef42a5aaceba670b3e266659804b1
lss <1043636133@qq.com>
2026-09-13 11:50:30 +0800

    chore: bump version to 1.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 
 
 [assembly: AssemblyTitle("LiveCaptions Translator")]
-[assembly: AssemblyVersion("1.7.1300.1822")]
-[assembly: AssemblyFileVersion("1.7.1300.1822")]
+[assembly: AssemblyVersion("1.8.1304.1311")]
+[assembly: AssemblyFileVersion("1.8.1304.1311")]
